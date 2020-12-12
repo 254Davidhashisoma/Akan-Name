fec7b8f5cafd31487cb5c46eec44a0e3329db6c3
254Davidhashisoma <davidhashisoma952gmail.com>
2020-12-12 19:08:17 +0300

    add js file

---
+++ script.js
@@ -0,0 +1,29 @@
+var maleAkanName = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+];
+
+var femaleAkanName = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
+
+var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
